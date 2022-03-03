a8ed5eb2ca48d34bfe46dda8bc6a2ddcb26b093b
Calabria <I559573@fvfx9225j1wv.station>
2022-03-03 19:52:34 +0100

    fixes

--- src/ai-models/predictive-maintenance/code/infer/infer.py
+++ src/ai-models/predictive-maintenance/code/infer/infer.py
@@ -8,24 +8,10 @@ from joblib import load
 import numpy as np
 from flask import Flask, request
 import librosa
-import librosa.display
-import wave
 
 app = Flask(__name__)
 
 sound_pipeline, target_classes = None, None
-
-def acoustic_feature_computation( clip ):
-    scale, sr = librosa.load(clip)
-    mel_spectrogram = librosa.feature.melspectrogram(scale, 
-                                                 sr, 
-                                                 hop_length=512,
-                                                 n_mels=64,
-                                                 fmax=sr/2)
-    log_mel = librosa.power_to_db(mel_spectrogram)
-    MFCCs=librosa.feature.mfcc(scale, sr, n_mfcc=40, fmax=sr/2)
-    acoustic_features=np.concatenate( (MFCCs,log_mel_spectrogram), axis =0)
-    return acoustic_features
 
 @app.before_first_request
 def init():
@@ -34,7 +20,7 @@ def init():
     """
     global sound_pipeline, target_classes
     path = "/mnt/models"
-    model_name = "ssound_classifier.pkl"
+    model_name = "sound_classifier.pkl"
 
     if exists(f"{path}/{model_name}"):
         print(f"Loading classifier pipeline from {path}")
@@ -45,6 +31,21 @@ def init():
         raise FileNotFoundError(f"{path}/{model_name}")
 
     return None
+
+
+def acoustic_feature_computation(clip):
+    scale, sr = librosa.load(clip)
+    mel_spectrogram = librosa.feature.melspectrogram(
+        scale, 
+        sr, 
+        hop_length=512,
+        n_mels=64,
+        fmax=sr/2)
+    log_mel = librosa.power_to_db(mel_spectrogram)
+    log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
+    MFCCs=librosa.feature.mfcc(scale, sr, n_mfcc=40, fmax=sr/2)
+    acoustic_features=np.concatenate( (MFCCs,log_mel_spectrogram), axis =0)
+    return acoustic_features
 
 
 @app.route("/v1/models/{}:predict".format("soundmodel"), methods=["POST"])
@@ -59,11 +60,11 @@ def predict():
     global sound_pipeline, target_classes
     input_data = dict(request.json)
 
-    acoustic_features = prepare_data(input_data)
     input_data = input_data["sound"]
     input_data = np.frombuffer(input_data, count = 33075, dtype = np.float32)
+    acoustic_features = acoustic_feature_computation(input_data)
 
-    prediction = sound_pipeline.predict([input_data])
+    prediction = sound_pipeline.predict([acoustic_features])
     predicted_label = "Anomalous" if prediction[0] > 0.5 else "Normal" #To be tuned
     #predicted_label = target_classes[prediction[0]]
     output = {"predictions": predicted_label}
