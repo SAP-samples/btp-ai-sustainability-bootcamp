f22e59971aa1bb4339d47bc4f7efeb3e353e7557
yatseali <yatseali@users.noreply.github.com>
2022-03-23 16:04:08 +1100

    add visual filter for anomalyType on eq-cond-alp

--- src/sustainable-smart-factory-app/srv/analytics-service.js
+++ src/sustainable-smart-factory-app/srv/analytics-service.js
@@ -2,9 +2,13 @@ const cds = require("@sap/cds");
 
 module.exports = async function () {
   const db = await cds.connect.to("db");
-  const { EquipmentView } = db.entities;
+  const { EquipmentView, AnomalyTypeNameView } = db.entities;
 
   this.on("READ", "EquipmentVH", async (req) => {
     return SELECT.from(EquipmentView);
+  });
+
+  this.on("READ", "AnomalyTypeNameVH", async (req) => {
+    return SELECT.from(AnomalyTypeNameView);
   });
 };
