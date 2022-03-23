e2a8794f79c59211a42eb84cbfbf5377230e4c57
Calabria <I559573@fvfx9225j1wv.station>
2022-03-23 17:00:40 +0100

    changing input format for the image inference

--- src/ai-models/defect-detection/code/infer/infer.py
+++ src/ai-models/defect-detection/code/infer/infer.py
@@ -8,6 +8,12 @@ from joblib import load
 import numpy as np
 from flask import Flask, request
 
+from base64 import b64encode
+import base64
+import io
+from json import dumps
+import cv2
+
 app = Flask(__name__)
 
 image_pipeline, target_classes = None, None
@@ -32,6 +38,17 @@ def init():
     return None
 
 
+def load_image(data):
+    IMG_WIDTH=224
+    IMG_HEIGHT=224
+    image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    image = cv2.resize(image, (IMG_HEIGHT, IMG_WIDTH),interpolation = cv2.INTER_AREA)
+    image = np.array(image)
+    image = image.astype('float32')
+    image /= 255
+    return image
+
+
 @app.route("/v1/models/{}:predict".format("imagemodel"), methods=["POST"])
 def predict():
     """
@@ -43,12 +60,17 @@ def predict():
 
     global image_pipeline, target_classes
     input_data = dict(request.json)
-    input_data = np.array(input_data["image"], np.float32)
 
-    prediction = image_pipeline.predict(input_data)
-    predicted_label = "Anomalous" if prediction[0] > 0.5 else "Normal" #To be tuned
-    #predicted_label = target_classes[prediction[0]]
-    output = {"predictions": predicted_label}
+    image_file_as_binary = base64.b64decode(input_data['image'])
+    nparr = np.frombuffer(image_file_as_binary, np.uint8)
+    x_inference = load_image(nparr)
+    b = np.array([np.array(x_inference)])
+
+    prediction = image_pipeline.predict(b)
+    predicted_output = prediction[0][0]
+    predicted_label = "Anomalous" if predicted_output > 0.5 else "Normal" #To be tuned
+    predicted_prob = predicted_output if predicted_output > 0.5 else (1-predicted_output)
+    output = {predicted_label:str(predicted_prob)}
 
     return output
 
