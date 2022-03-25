326eddea4b585c1738ecf42ab39d48d844d9a6d5
amagnani <alice.magnani@gmail.com>
2022-03-25 14:38:18 +0100

    Build Factory based on yaml configuration file

--- src/data-simulation/plantSimulation.py
+++ src/data-simulation/plantSimulation.py
@@ -15,12 +15,7 @@ class FactorySimulation():
         self._cyclicMaintFreq = 28 #in days
         self._MaintDuration = 24 #in hours
         self._cyclicMaintList=[]
-        self._cyclicMaintClock=0
-        self._downtimesClock={}
-        self._faultClock={}
         self._RepairDuration = 5*24 #in hours
-        self.nPlants=2
-        self.nMachines=5
         self._corrective_nr = 1
         self._predictive_nr = 1
         self._cyclic_nr = 1
@@ -29,23 +24,8 @@ class FactorySimulation():
         self.cfg= 'cfg/machine1.yaml'
         self._outfile1 = 'sim_'+factory_nr+'.csv'
         self._outfile2 = 'maintenance_'+factory_nr+'.csv'
+        self.t = self._cyclicStartDate
 
-
-    # Populate the factory with plants and machines and create the clock
-    def build_factory(self):
-        for p in range(1,self.nPlants+1):
-            p_name='p'+str(p)
-            P = plant.Plant(p_name)
-            self._downtimesClock[p_name]={}
-            self._faultClock[p_name]={}
-            self._factory.add_plants(P)
-            for m in range(1,self.nMachines+1):
-                m_name='m'+str(m)
-                M = plant.Machine(m_name, self.cfg)
-                P.add_machine(M)
-                self._downtimesClock[p_name][m_name]=0
-                self._faultClock[p_name][m_name]=0
-        return
 
     # Populate the factory with plants and machines and create the clock
     def draw_factory(self):
@@ -54,8 +34,8 @@ class FactorySimulation():
         fig_width = 20  # cm
         max_fig_height = 20  # cm
         height_per_plant = 5
-        width_per_machines = fig_width / self.nMachines
-        fig_height= height_per_plant * self.nPlants
+        width_per_machines = fig_width / np.max([ len(p._machine_dict.keys()) for p in self._factory._plants_dict.values() ] )
+        fig_height= height_per_plant * len(self._factory._plants_dict.keys())
         if fig_height > max_fig_height:
             fig_height = max_fig_height
         fig = plt.figure(figsize=(fig_width / 2.54, fig_height / 2.54))
@@ -81,7 +61,7 @@ class FactorySimulation():
         plants_titles_coordinates=[]
         plants_titles=[]
 
-        plant_counter=self.nPlants-1
+        plant_counter=len(self._factory._plants_dict.keys())-1
         radii = min(height_per_plant,width_per_machines) * 0.3
         for kp,p in self._factory._plants_dict.items():
             plants_titles_coordinates.append((fig_width*0.1, height_per_plant* 0.9  + height_per_plant * plant_counter))
@@ -112,7 +92,7 @@ class FactorySimulation():
                 verticalalignment="center",
                 color="black",
                 )
-            for i in range(self.nMachines-1):
+            for i in range(len(p._machine_dict.keys())-1):
                 ax.annotate(
                     "",
                 (machines_coordinates[i+1][0] - radii, machines_coordinates[i+1][1]),
@@ -184,7 +164,7 @@ class FactorySimulation():
             if proactive:
                 # set the fault clock to -1: the maintenance hasn't started yet.
                 #The AI algo will recognise the fault and schedule the machine within few hours
-                self._faultClock[plant._plant_nr][machine._equipment_nr]=-1-self._proactivness
+                self._factory._faultClock[plant._plant_nr][machine._equipment_nr]=-1-self._proactivness
         return
 
     # Function to generate breakdown on a certain machine
@@ -195,14 +175,29 @@ class FactorySimulation():
             machine._set_status('BROKEN')
             plant._turnOff()
             plant._set_status()
-            self._downtimesClock[plant._plant_nr][machine._equipment_nr]=self._RepairDuration
+            self._factory._downtimesClock[plant._plant_nr][machine._equipment_nr]=self._RepairDuration
             return 1
         else:
             return 0
 
     # Function to bring up a plant after a corrective maintenance
-    def corrective_maintenance( self, plant, machine):
-        if self._downtimesClock[plant._plant_nr][machine._equipment_nr] == 1 :
+    def corrective_maintenance( self, plant, machine, f2):
+        if self._factory._downtimesClock[plant._plant_nr][machine._equipment_nr] == 1 :
+
+            #Print maintenance
+            start_time=str(self.t-timedelta(hours=self._RepairDuration))
+            end_time=str(self.t)
+            maintenance_nr='CORRECTIVE'+str(self._corrective_nr).zfill(5)
+            maintenance_str=start_time + ',' + end_time + ',' +\
+                            plant._plant_nr + ',' +\
+                            machine._equipment_nr + ',' +\
+                            maintenance_nr + ',' +\
+                            str(machine._statusParameters['maintenance_cost']) + ',' +\
+                            str(self._RepairDuration) + '\n'
+            print('status',machine._status,machine._statusParameters['maintenance_cost'])
+            f2.write(maintenance_str)
+
+            #Turn machine up
             machine._set_status('OK')
             plant._turnOn()
             plant._set_status()
@@ -212,12 +207,35 @@ class FactorySimulation():
     # Function to :
     # - stop the environment for a maintenance
     # - bring up a plant after a "predictive" maintenance
-    def predictive_maintenance( self, plant, machine):
-        if self._faultClock[plant._plant_nr][machine._equipment_nr] == -1 :
+    def predictive_maintenance( self, plant, machine, f2):
+
+        #if there has been a breakdown, forget about the predictive maintenance, you're late
+        if self._factory._faultClock[plant._plant_nr][machine._equipment_nr] !=0 and self._factory._downtimesClock[plant._plant_nr][machine._equipment_nr]>0:
+            self._factory._faultClock[plant._plant_nr][machine._equipment_nr]=0
+
+        # shut down the plant for a proactive maintenance if it is time to do so
+        if self._factory._faultClock[plant._plant_nr][machine._equipment_nr] == -1 :
             plant._turnOff()
             plant._set_status()
-            self._faultClock[plant._plant_nr][machine._equipment_nr] = self._MaintDuration
-        if self._faultClock[plant._plant_nr][machine._equipment_nr] == 1 :
+            self._factory._faultClock[plant._plant_nr][machine._equipment_nr] = self._MaintDuration
+
+        # bring everything up if the proactive maintenance is done
+        if self._factory._faultClock[plant._plant_nr][machine._equipment_nr] == 1 :
+
+            #Print maintenance
+            start_time=str(self.t-timedelta(hours=self._MaintDuration))
+            end_time=str(self.t)
+
+            maintenance_nr='PROACTIVE'+str(self._predictive_nr).zfill(5)
+            maintenance_str=start_time + ',' + end_time + ',' +\
+                            plant._plant_nr + ',' +\
+                            machine._equipment_nr + ',' +\
+                            maintenance_nr + ',' +\
+                            str(machine._statusParameters['maintenance_cost']) + ',' +\
+                            str(self._MaintDuration) + '\n'
+            f2.write(maintenance_str)
+
+            #Turn machine up
             machine._set_status('OK')
             plant._turnOn()
             plant._set_status()
@@ -225,14 +243,35 @@ class FactorySimulation():
         return
 
     # Function to bring up the factory after a corrective maintenance
-    def cyclic_maintenance(self):
-        if self._cyclicMaintClock==1:
-            for p in self._factory._plants_dict.values():
-                for m in p._machine_dict.values():
-                    m._set_status('OK')
-                p._turnOn()
-                p._set_status()
-                self._cyclic_nr = self._cyclic_nr+1
+    def cyclic_maintenance(self, plant, machine, f2):
+
+        if self._factory._cyclicMaintClock[plant._plant_nr][machine._equipment_nr] !=0 and self._factory._downtimesClock[plant._plant_nr][machine._equipment_nr]>0:
+            self._factory._cyclicMaintClock[plant._plant_nr][machine._equipment_nr]=0
+
+        if self._factory._cyclicMaintClock[plant._plant_nr][machine._equipment_nr]==1:
+            #Print maintenance
+            start_time=str(self.t-timedelta(hours=self._MaintDuration))
+            end_time=str(self.t)
+            maintenance_nr='CYCLIC'+str(self._cyclic_nr).zfill(5)
+            maintenance_str=start_time + ',' + end_time + ',' +\
+                            plant._plant_nr + ',' +\
+                            machine._equipment_nr + ',' +\
+                            maintenance_nr + ',' +\
+                            str(machine._statusParameters['maintenance_cost']) + ',' +\
+                            str(self._MaintDuration) + '\n'
+            f2.write(maintenance_str)
+
+            #Turn machine up
+            machine._set_status('OK')
+            self._cyclic_nr = self._cyclic_nr+1
+            all_good=1
+            for m in plant._machine_dict.values():
+                if m._status=='BROKEN':
+                    all_good=0
+                    break
+            if all_good:
+                 plant._turnOn()
+            plant._set_status()
         return
 
     def run(self):
@@ -244,37 +283,38 @@ class FactorySimulation():
         sep=','
 
         ### Start the simulation
-        t=self._cyclicStartDate
         D=timedelta(hours=1)
         proactive = 0
 
-        while t < self._proactiveEndDate:
-            t =t + D
+        while self.t < self._proactiveEndDate:
+            self.t =self.t + D
 
-            if t > self._cyclicEndDate :
+            if self.t > self._cyclicEndDate :
                 proactive=1
 
             #Update clocks
             # cyclic maintenance
-            if self._cyclicMaintClock>0:
-                self._cyclicMaintClock=self._cyclicMaintClock -1
+            for kp, vp in self._factory._cyclicMaintClock.items():
+                for km, vm in vp.items():
+                    if vm > 0:
+                        self._factory._cyclicMaintClock[kp][km]=vm-1
+                    # Was a cyclic maintenance scheduled for today? if so, turn the factory off
+                    elif vm == 0 and self.t.strftime('%Y-%m-%d') in [ d.strftime('%Y-%m-%d') for d in self._cyclicMaintList]:
+                        self._factory._cyclicMaintClock[kp][km] = self._MaintDuration
+                        self._factory._turnOff()
             # corrective maintenance
-            for kp, vp in self._downtimesClock.items():
+            for kp, vp in self._factory._downtimesClock.items():
                 for km, vm in vp.items():
                     if vm > 0:
-                        self._downtimesClock[kp][km]=vm-1
+                        self._factory._downtimesClock[kp][km]=vm-1
             # predictive maintenance
-            for kp, vp in self._faultClock.items():
+            for kp, vp in self._factory._faultClock.items():
                 for km, vm in vp.items():
                     if vm > 0:
-                        self._faultClock[kp][km]=vm-1
+                        self._factory._faultClock[kp][km]=vm-1
                     if vm < 0:
-                        self._faultClock[kp][km]=vm+1
+                        self._factory._faultClock[kp][km]=vm+1
 
-            # Was a cyclic maintenance scheduled for today? if so, turn the factory off
-            if self._cyclicMaintClock == 0 and t.strftime('%Y-%m-%d') in [ d.strftime('%Y-%m-%d') for d in self._cyclicMaintList]:
-                self._cyclicMaintClock = self._MaintDuration
-                self._factory._turnOff()
 
             # Extract current energy consumption and yield value of the machines according to gaussian ditributions
             self.measure_generator()
@@ -290,30 +330,30 @@ class FactorySimulation():
 
 
                     #Execute maintenance, if needed
-                    self.corrective_maintenance(p,m)
-                    self.predictive_maintenance(p,m)
-                    self.cyclic_maintenance()
+                    self.corrective_maintenance(p,m,f2)
+                    self.predictive_maintenance(p,m,f2)
+                    self.cyclic_maintenance(p,m,f2)
 
                     #Print the status of each machine
-                    f1.write(str(t))
+                    f1.write(str(self.t))
                     f1.write(sep)
                     f1.write(p._print_status())
                     f1.write(sep)
                     f1.write(m._print_status())
                     f1.write(sep)
-                    if self._cyclicMaintClock > 1 :
+                    if self._factory._cyclicMaintClock[p._plant_nr][m._equipment_nr] > 1 :
                         maintenance_nr='CYCLIC'+str(self._cyclic_nr).zfill(5)
                         f1.write(maintenance_nr)
                         f1.write(sep)
                     else:
                         f1.write(sep)
-                    if self._downtimesClock[p._plant_nr][m._equipment_nr] > 1 :
+                    if self._factory._downtimesClock[p._plant_nr][m._equipment_nr] > 1 :
                         maintenance_nr='CORRECTIVE'+str(self._corrective_nr).zfill(5)
                         f1.write(maintenance_nr)
                         f1.write(sep)
                     else:
                         f1.write(sep)
-                    if self._faultClock[p._plant_nr][m._equipment_nr] > 1 :
+                    if self._factory._faultClock[p._plant_nr][m._equipment_nr] > 1 :
                         maintenance_nr='PROACTIVE'+str(self._predictive_nr).zfill(5)
                         f1.write(maintenance_nr)
                         f1.write('\n')
@@ -326,7 +366,7 @@ class FactorySimulation():
 
 def main():
         Sim = FactorySimulation('LGP_factory')
-        Sim.build_factory()
+        Sim._factory._build_from_yaml('cfg/LGP_factory.yaml')
         Sim.draw_factory()
         Sim._factory._turnOn()
         Sim.schedule_cyclic_maintenance()
