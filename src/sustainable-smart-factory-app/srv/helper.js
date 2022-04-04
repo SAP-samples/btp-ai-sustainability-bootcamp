612d2d9db2e872fc7d0581774f31fb8a35ecca61
Jacob Tan <jacobtan89@gmail.com>
2022-04-04 17:25:24 +0800

    mo desc improvements, eqcondquery projection from catservice, test case http

--- src/sustainable-smart-factory-app/srv/helper.js
+++ src/sustainable-smart-factory-app/srv/helper.js
@@ -4,24 +4,18 @@ const {
 const { maintenanceOrderApi } = maintenanceOrderService();
 
 const _prepareMoBody = (mo) => {
-  function randomIntFromInterval(min, max) {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
   const opsDuration = randomIntFromInterval(1, 8);
-
   return {
     maintenanceOrderType: mo.OrderType,
     equipment: mo.Equipment,
-    maintPriority: mo.MaintPriority,
-    maintenanceOrderDesc: mo.Desc,
+    maintPriority: randomIntFromInterval(1,4),
+    maintenanceOrderDesc: limit(mo.Desc, 40),
     responsibleCostCenter: mo.CostCenter,
     toMaintenanceOrderOperation: [
       {
         maintenanceOrderOperation: "0010",
         operationControlKey: "YBM1",
-        operationDescription: "Fix " + mo.EquipmentName,
+        operationDescription: limit(mo.OperationDesc),
         maintOrderOperationDuration: opsDuration,
         maintOrdOperationDurationUnit: "H",
         maintOrdOperationWorkDuration: opsDuration,
@@ -32,6 +26,15 @@ const _prepareMoBody = (mo) => {
     ],
   };
 };
+
+function limit(string = "", limit = 0) {
+  return string.substring(0, limit);
+}
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 const buildMaintenanceOrderForCreate = (data) => {
   const mo = maintenanceOrderApi.entityBuilder().fromJson(_prepareMoBody(data));
