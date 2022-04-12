4cd17a92cfeb7ca030c99a00b9749b318c5c563c
Jacob Tan <jacobtan89@gmail.com>
2022-04-12 22:07:21 +0800

    visualisation app initial

---
+++ src/sustainable-smart-factory-app/app/predictive-maintenance/plant-conditions-visualisation/webapp/plant-visual/languages.js
@@ -0,0 +1,633 @@
+document.lang_english = {
+	title: "Plant Conditions",
+	languages: {
+		de: "German",
+		en: "English"
+	},
+	segments: {
+		all: "All Equipments",
+		dry: "LGP Cutting Mach 1",
+		drugs: "LGP Cutting Mach 2",
+		milk: "LED Assem Mac Robot 1",
+		frozen: "LED Assem Mac Robot 2",
+		meat: "LED Assem Mac Robot 3",
+		veg: "LED Assem Mac Robot 4",
+		magazines: "LED Assem Mac Robot 5",
+		convenience: "LED Assem Mac Robot 6"
+	},
+	sections: {
+		overview: "Plant overview",
+		actions: "Equipment analysis",
+		area: "Energy analysis",
+		customer: "Maintenance typologies"
+	},
+	actionarea: "Promotion area",
+	overviewsection: {
+		revenue: "Revenue, current week:",
+		revenueComparison: "Revenue current day in k€ (comparison)",
+		rawRevenueComparison: "Gross profit current day in k€ (comparison)"
+	},
+	actionssection: {
+		cartPart: "Share of promotional merchandise in average shopping cart",
+		salesrate: "(Sales rate/revenue)",
+		month: "current Month"
+	},
+	areasection: {
+		revenue: "Revenue comparison by square meter",
+		customersPerHour: "Customers per hour",
+		customersPerDay: "Customers per day"
+	},
+	customersection: {
+		convenienceAnnotation: "Convenience product offerings recommended",
+		statistic: "Customer statistics",
+		bouncerate: "Bounce rate",
+		bouncerateDescription: "Dwell time under 1 minute ",
+		totalCustomers: "Total customers",
+		totalVisits: "Total visits",
+		totalVisitsDescription: "Total number of visitors",
+		visitsPerCustomer: "Visits per customer",
+		visitsPerCustomerDescription: "Avg. no. of in-store visits",
+		dwellTime: "Average dwell time",
+		dwellTimeDescription: "Average dwell time per customer",
+		cartDay: "Daily shopping cart breakdown",
+		cartHour: "Hourly shopping cart breakdown"
+	},
+	notifications: {
+		notifications: "Notifications",
+		messages: {
+			convenienceOffer: "Convenience product offerings recommended between 5:00 and 6:00 p.m.",
+			convenienceChange: "Order for convenience product sale has been placed",
+			dry: "Share of promotional merchandise (dry goods) in shopping cart is below average",
+			dryChange: "Promotion area of dry goods has been adjusted",
+			veg: "Shopfloor productivity declining in fruits and vegetables",
+			vegChange: "Promotion area of fruits and vegetables has been adjusted"
+		}
+	},
+	date: {
+		day: "dd",
+		notification: "dddd DD.MM.YYYY, HH:mm",
+		main: "dddd DD.MM.YYYY",
+		date: "DD.MM.YYYY"
+	},
+	units: {
+		teur: "k€",
+		eur: "€",
+		piece: "pieces",
+		kg: "Kg",
+		bowl250: "250g bowls"
+	},
+	buttons: {
+		save: "Trigger Support",
+		cancel: "Cancel"
+	},
+	heatmap: {
+		customers: "Customers per hour",
+		annotations: {
+			dry: "Anomaly#1 LGP Prod Line#1. Abnormal sound detected",
+			veg: "Shopfloor productivity declining in promotion area"
+		}
+	},
+	message: {
+		title: "Edit promotion areas",
+		saved: "Message to:",
+		position: "Team lead",
+		message: "Dear Mr. Doe,\n\nPlease adjust the promotion area as indicated.\n\nBest regards,\n\nHans Meier\nManaging Director"
+	},
+	marketingMessage: {
+		title: "Execute Marketing Activity",
+		saved: "Message to:",
+		position: "Marketing manager",
+		message: "Please start a coupon campaign for the selected\ncustomer segment:\n\nBuy two salads, get a free smoothie.",
+		currentAction: "Current activity",
+		smoothieStand: "Smoothie booth",
+		currentDay: "Sales ranking, current day",
+		lastWeek: "Sales ranking, current week",
+		pieces: "No. units",
+		layout: "Please choose a layout"
+	},
+	products: {
+		greencabbage: "Kale",
+		apples: "Apples",
+		bananas: "Bananas",
+		ketchup: "Scratches",
+		olives: "EQ 3",
+		mango: "Mangos",
+		rice: "EQ 6",
+		berrymix: "Berry mix",
+		cucumbers: "Cucumbers",
+		hellespesto: "EQ 4",
+		noodles: "EQ 5",
+		sauce: "EQ 7",
+		tea: "EQ 8",
+		smoothiestand: "Smoothie booth",
+		pickles: "Cracks",
+		lime: "Limes",
+		redcabbage: "Beetroot",
+		oranges: "Oranges"
+	},
+	editArea: {
+		edit: "Alert:",
+		addButton: "Not detected visually",
+		removeButton: "Detected visually",
+		currentSalesRate: "Yield rate during current production",
+		lastSalesRate: "Yield rate during last production",
+		currentRevenue: "Costs, current production",
+		lastRevenue: "Costs, previous production",
+		stock: "Production level",
+		pieces: "No. units",
+		sales: "No. units produced",
+		supplier: "Engineer assigned",
+		supplyDate: "Planned maintenance"
+	},
+	cities: {
+		frankfurt: "0095 Frankfurt",
+		munich: "8934 Munich",
+		berlin: "1091 Berlin",
+		dresden: "0106 Dresden"
+	},
+	barchart: {
+		is: "Actual figures",
+		plan: "Planned figures"
+	},
+	radarchart: {
+		days: ["Mo", "Tu", "We", "Th", "Fr", "Sa"]
+	},
+	linechart: {
+		lastYear: "Previous year",
+		plan: "Planned",
+		current: "Actual",
+		loss: "Operating at a loss"
+	},
+	toplow: {
+		rice: "Rice",
+		noodles: "Noodles",
+		ketchup: "Ketchup",
+		olives: "Olives",
+		sauce: "Sauce",
+		pickles: "Pickles",
+		perfume: "Perfume",
+		deodorant: "Deodorant",
+		makeup: "Makeup",
+		tissues: "Tissues",
+		toothbrush: "Toothbrushes",
+		showergel: "Shower gel",
+		parmesan: "Parmesan",
+		mozzarella: "Mozzarella",
+		cheese: "Cheese",
+		joghurt: "Yogurt",
+		milk: "Milk",
+		camembert: "Camembert",
+		pizza: "Pizza",
+		chicken: "Chicken",
+		icecream: "Ice cream",
+		frenchfries: "French fries",
+		vegetables: "Vegetables",
+		potatoes: "Potatoes",
+		sausages: "Cold cuts",
+		burgers: "Burgers",
+		pig: "Pork",
+		beef: "Beef",
+		steak: "Steak",
+		apples: "Apples",
+		cherry: "Cherries",
+		banana: "Bananas",
+		mango: "Mangos",
+		oranges: "Oranges",
+		berrymix: "Berry mix",
+		diymag: "DIY magazine",
+		sportsillustrated: "Sports magazine",
+		businessmanager: "Business manager magazine",
+		holisticsoul: "Holistic Soul magazine",
+		intermediate: "Intermediate magazine",
+		petshop: "Pet shop magazine",
+		ramen: "Ramen noodles",
+		chocolate: "Chocolate",
+		instantnoodles: "Instant noodles",
+		bubblegum: "Chewing gum",
+		jellybaby: "Gummi bears",
+		candy: "Candy"
+	}
+};
+
+document.lang_german = {
+	title: "Megamarkt Frankfurt",
+	languages: {
+		de: "Deutsch",
+		en: "Englisch"
+	},
+	segments: {
+		all: "Alle Bereiche",
+		dry: "Trockensortiment",
+		drugs: "Drogeriewaren",
+		milk: "Milchprodukte",
+		frozen: "Tiefkühlkost",
+		meat: "Fleisch und Wurst",
+		veg: "Obst und Gemüse",
+		magazines: "Zeitschriften",
+		convenience: "Convenience"
+	},
+	sections: {
+		overview: "Filialübersicht",
+		actions: "Aktionsanalyse",
+		area: "Flächenanalyse",
+		customer: "Kundentypologie"
+	},
+	actionarea: "Aktionsfläche",
+	overviewsection: {
+		revenue: "Umsatz aktuelle Woche:",
+		revenueComparison: "Umsatz aktueller Tag T€ (Vergleich)",
+		rawRevenueComparison: "Rohertrag aktueller Tag T€ (Vergleich)"
+	},
+	actionssection: {
+		cartPart: "Anteil Werbeware am durchschnittlichen Warenkorb",
+		salesrate: "(Abverkaufsrate/Umsatz)",
+		month: "Monat aktuell"
+	},
+	areasection: {
+		revenue: "Umsatz pro Quadratmeter im Vergleich",
+		customersPerHour: "Kunden pro Stunde",
+		customersPerDay: "Kunden pro Tag"
+	},
+	customersection: {
+		convenienceAnnotation: "Angebot für Convenience Produkte empfohlen",
+		statistic: "Kundenstatistik",
+		bouncerate: "Bounce Rate",
+		bouncerateDescription: "Verweildauer unter 1 Minute",
+		totalCustomers: "Kunden gesamt",
+		totalVisits: "Besuche gesamt",
+		visitsPerCustomer: "Anzahl Besuche/Kunde",
+		dwellTime: "durchschnittliche Verweildauer",
+		dwellTimeDescription: "Verweildauer pro Kunde",
+		cartDay: "Zusammensetzung Warenkorb pro Tag",
+		cartHour: "Zusammensetzung Warenkorb pro Stunde"
+	},
+	notifications: {
+		notifications: "Benachrichtigungen",
+		messages: {
+			convenienceOffer: "Angebot für Convenience Produkte zwischen 17 und 18 Uhr empfohlen",
+			convenienceChange: "Angebot für Convenience Produkte wurde in Auftrag gegeben",
+			dry: "Anteil der Werbeware Trockensortiment am Warenkorb unterdurchschnittlich",
+			dryChange: "Aktionsfläche Trockensortiment wurde umgestellt",
+			veg: "Flächenproduktivität im Obst- und Gemüsebereich rückläufig",
+			vegChange: "Aktionsfläche Obst- und Gemüse wurde umgestellt"
+		}
+	},
+	date: {
+		day: "dd",
+		notification: "dddd DD.MM.YYYY, HH:mm [Uhr]",
+		main: "dddd DD.MM.YYYY",
+		date: "DD.MM.YYYY"
+	},
+	units: {
+		teur: "T€",
+		eur: "€",
+		piece: "Stück",
+		kg: "Kg",
+		bowl250: "250g Schalen"
+	},
+	buttons: {
+		save: "Speichern",
+		cancel: "Abbrechen"
+	},
+	heatmap: {
+		customers: "Kunden pro Stunde",
+		annotations: {
+			dry: "Aktionsproduktivität rückläufig",
+			veg: "Flächenproduktivität auf der Aktionsfläche rückläufig"
+		}
+	},
+	message: {
+		title: "Aktionsflächen bearbeiten",
+		saved: "Benachrichtigung an:",
+		position: "Teamleiter",
+		message: "Sehr geehrter Herr Mustermann,\n\nbitte ändern Sie die Aktionsfläche wie angegeben.\n\nMit freundlichen Grüßen,\n\nHans Meier\nGeschäftsleiter"
+	},
+	marketingMessage: {
+		title: "Marketingmaßnahme durchführen",
+		saved: "Benachrichtigung an:",
+		position: "Marketing-Manager",
+		message: "Bitte starten Sie eine Coupon-Kampagne für das ausgewählte\nKundensegment:\n\n2 Salate kaufen, 1 Smoothie gratis.",
+		currentAction: "Laufende Maßnahme",
+		smoothieStand: "Smoothiestand",
+		currentDay: "Verkaufsrangliste aktueller Tag",
+		lastWeek: "Verkaufsrangliste aktuelle Woche",
+		pieces: "Stück",
+		layout: "Wählen Sie ein Layout"
+	},
+	products: {
+		greencabbage: "Grünkohl",
+		apples: "Äpfel",
+		bananas: "Bananen",
+		ketchup: "Ketchup",
+		olives: "Oliven",
+		mango: "Mangos",
+		rice: "Reis",
+		berrymix: "Beerenmix",
+		cucumbers: "Gurken",
+		hellespesto: "Helles Pesto",
+		noodles: "Nudeln",
+		sauce: "Soße",
+		tea: "Tee",
+		smoothiestand: "Smoothiestand",
+		pickles: "Gewürzgurken",
+		lime: "Limetten",
+		redcabbage: "Rote Beete",
+		oranges: "Orangen"
+	},
+	editArea: {
+		edit: "Aktionsfläche bearbeiten:",
+		addButton: "Auf der Aktionsfläche platzieren",
+		removeButton: "Von der Aktionsfläche entfernen",
+		currentSalesRate: "Abverkaufsrate aktueller Aktionszeitraum",
+		lastSalesRate: "Abverkaufsrate letzter Aktionszeitraum",
+		currentRevenue: "Umsatz aktuelle Aktion",
+		lastRevenue: "Umsatz letzte Aktion",
+		stock: "Lagerbestand",
+		pieces: "Stück",
+		sales: "Verkaufte Stückzahl",
+		supplier: "Lieferant",
+		supplyDate: "geplanter Wareneingang"
+	},
+	cities: {
+		frankfurt: "0095 Frankfurt",
+		munich: "8934 München",
+		berlin: "1091 Berlin",
+		dresden: "0106 Dresden"
+	},
+	barchart: {
+		is: "Ist-Zahlen",
+		plan: "Plan-Zahlen"
+	},
+	radarchart: {
+		days: ["Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+	},
+	linechart: {
+		lastYear: "Vorjahr",
+		plan: "Plan",
+		current: "Ist",
+		loss: "Verlustzone"
+	},
+	toplow: {
+		rate: "Abverkaufsrate",
+		revenue: "Umsatz",
+		rice: "Reis",
+		noodles: "Nudeln",
+		ketchup: "Ketchup",
+		olives: "Oliven",
+		sauce: "Soße",
+		pickles: "Gewürzgurken",
+		perfume: "Parfum",
+		deodorant: "Deodorant",
+		makeup: "Makeup",
+		tissues: "Tücher",
+		toothbrush: "Zahnbürsten",
+		showergel: "Duschgel",
+		parmesan: "Parmesan",
+		mozzarella: "Mozzarella",
+		cheese: "Käse",
+		joghurt: "Joghurt",
+		milk: "Milch",
+		camembert: "Camembert",
+		pizza: "Pizza",
+		chicken: "Hühnchen",
+		icecream: "Eis",
+		frenchfries: "Pommes",
+		vegetables: "Gemüse",
+		potatoes: "Kartoffeln",
+		sausages: "Wurst",
+		burgers: "Burger",
+		pig: "Schwein",
+		beef: "Rind",
+		steak: "Steak",
+		apples: "Äpfel",
+		cherry: "Kirschen",
+		banana: "Bananen",
+		mango: "Mangos",
+		oranges: "Orangen",
+		berrymix: "Beerenmix",
+		diymag: "DIY Magazin",
+		sportsillustrated: "Sportmagazin",
+		businessmanager: "Businessmanager",
+		holisticsoul: "Holisticsoul",
+		intermediate: "Intermediate",
+		petshop: "Petshop",
+		ramen: "Ramen",
+		chocolate: "Schokolade",
+		instantnoodles: "Instant Nudeln",
+		bubblegum: "Kaugummi",
+		jellybaby: "Gummibären",
+		candy: "Süßigkeiten",
+		greencabbage: "Grünkohl"
+	}
+};
+
+document.lang_port = {
+	title: "Megamercado São Paulo",
+	languages: {
+		de: 'Português (BR)',
+		en: 'Inglês'
+	},
+	segments: {
+		all: 'Todas as áreas',
+		dry: 'Bens secos',
+		drugs: 'Produtos de farmácia',
+		milk: 'Laticínios',
+		frozen: 'Comida congelada',
+		meat: 'Produtos de carnes',
+		veg: 'Frutas e vegetais',
+		magazines: 'Publicações',
+		convenience: 'Produtos de conveniência'
+	},
+	sections: {
+		overview: 'Visão geral da loja',
+		actions: 'Análise de promoção',
+		area: 'Análise chão de loja',//'Análise de chão de fábrica',
+		customer: 'Tipologias de cliente'
+	},
+	actionarea: 'Área de promoção',
+	overviewsection: {
+		revenue: 'Receita da semana atual:',
+		revenueComparison: 'Receita do dia atual em kBRL (comparação)',
+		rawRevenueComparison: 'Lucro bruto do dia atual kBRL (comparação)'
+	},
+	actionssection: {
+		cartPart: 'Percentual de mercadorias promocionais em média no carrinho de compra',
+		salesrate: '(taxa de vendas/receita)',
+		month: 'mês atual'
+	},
+	areasection: {
+		revenue: 'Comparação de receita por metro quadrado',
+		customersPerHour: 'Clientes por hora',
+		customersPerDay: 'Clientes por dia'
+	},
+	customersection: {
+		convenienceAnnotation: 'Ofertas de produtos de conveniência recomendados',
+		statistic: 'Estatística de cliente',
+		bouncerate: 'Taxa de rejeição',
+		bouncerateDescription: 'Recuperação de informação abaixo de 1 minuto ',
+		totalCustomers: 'Total de clientes',
+		totalVisits: 'Total de visitas',
+		totalVisitsDescription: 'Número total de visitantes',
+		visitsPerCustomer: 'Visitas por cliente',
+		visitsPerCustomerDescription: 'Média no número de visitas em loja',
+		dwellTime: 'Tempo de recuperação de informação médio',
+		dwellTimeDescription: 'Tempo de recuperação de informação médio por cliente',
+		cartDay: 'Detalhes do carrinho de compras por hora',
+		cartHour: 'Detalhes do carrinho de compras por dia'
+	},
+	notifications: {
+		notifications: 'Notificações',
+		messages: {
+			convenienceOffer: 'Ofertas de produto de conveniência recomendadas entre 17:00h e 18:00h',
+			convenienceChange: 'O pedido para venda de produto de conveniência foi enviado',
+			dry: 'Percentagem de mercadoria promocional (produtos secos) no carrinho de compras está abaixo da média',
+			dryChange: 'A área de promoção de produtos secos foi ajustado',
+			veg: 'Produtividade do chão de fábrica caindo em frutas e vegetais',
+			vegChange: 'Área de promoção de frutas e vegetais foi ajustada'
+		}
+	},
+	date: {
+		day: 'dd',
+		notification: 'dddd DD.MM.AAAA HH:mm',
+		main: 'dddd DD.MM.AAAA',
+		date: 'DD.MM.AAAA'
+	},
+	units: {
+		teur: 'kBRL',
+		eur: 'BRL',
+		piece: 'unidades',
+		kg: 'Kg',
+		bowl250: '250g tigelas'
+	},
+	buttons: {
+		save: 'Gravar',
+		cancel: 'Cancelar'
+	},
+	heatmap: {
+		customers: 'Clientes por hora',
+		annotations: {
+			dry: 'Produtividade promocional caindo',
+			veg: 'Produtividade do chão de fábrica caindo na área promocional'
+		}
+	},
+	message: {
+		title: 'Editar áreas promocionais',
+		saved: 'Mensagem para:',
+		position: 'Líder de equipe',
+		message: 'Prezado Senhor Doe, ajuste a área de promoção conforme indicado. Atenciosamente Hans Meier Diretor-Executivo'
+	},
+	marketingMessage: {
+		title: 'Executar atividade de marketing',
+		saved: 'Mensagem para:',
+		position: 'Gerente de marketing',
+		message: 'Inicie uma campanha de cupom para, customer segment: Compre duas saladas e ganhe um smoothie.',
+		currentAction: 'Atividade atual',
+		smoothieStand: 'Stand do smoothie',
+		currentDay: 'Classificação de vendas dia atual',
+		lastWeek: 'Classificação de vendas semana atual',
+		pieces: 'Unidades',
+		layout: 'Escolha um layout'
+	},
+	products: {
+		greencabbage: 'Couve',
+		apples: 'Maçãs',
+		bananas: 'Bananas',
+		ketchup: 'Ketchup',
+		olives: 'Azeitonas',
+		mango: 'Mangas',
+		rice: 'Arroz',
+		berrymix: 'Mistura de frutas vermelhas',
+		cucumbers: 'Pepinos',
+		hellespesto: 'Pesto light',
+		noodles: 'Macarrão',
+		sauce: 'Molho',
+		tea: 'Chá',
+		smoothieStand: 'Stand do smoothie',
+		pickles: 'Pickles',
+		lime: 'Limões',
+		redcabbage: 'Beterraba',
+		oranges: 'Laranjas'
+	},
+	editArea: {
+		edit: 'Editar área de promoção:',
+		addButton: 'Adicionar à área de promoção',
+		removeButton: 'Remover da área de promoção',
+		currentSalesRate: 'Taxa de vendas durante promoção atual',
+		lastSalesRate: 'Taxa de vendas durante última promoção',
+		currentRevenue: 'Receita da promoção atual',
+		lastRevenue: 'Receita da promoção anterior',
+		stock: 'Nível de estoque',
+		pieces: 'Unidades',
+		sales: 'Unidades vendidas',
+		supplier: 'Fornecedor',
+		supplyDate: 'Recebimento planejado de mercadorias'
+	},
+	cities: {
+		frankfurt: "0095 São Paulo",
+		munich: "8934 Brasília",
+		berlin: "1091 Rio de Janeiro",
+		dresden: "0106 Salvador"
+	},
+	barchart: {
+		is: 'Índices atuais',
+		plan: 'Índices planejados'
+	},
+	radarchart: {
+		days: ['Se', 'Te', 'Qa', 'Qi', 'Sx', 'Sa']
+	},
+	linechart: {
+		lastYear: 'Ano anterior',
+		plan: 'Planejado',
+		current: 'Real',
+		loss: 'Operacional em uma perda'
+	},
+	toplow: {
+		rice: 'Arroz',
+		noodles: 'Macarrão',
+		ketchup: 'Ketchup',
+		olives: 'Azeitonas',
+		sauce: 'Molho',
+		pickles: 'Pickles',
+		perfume: 'Perfume',
+		deodorant: 'Desodorante',
+		makeup: 'Maquiagem',
+		tissues: 'Lenços',
+		toothbrush: 'Escovas de dentes',
+		showergel: 'Gel de banho',
+		parmesan: 'Parmesão',
+		mozzarella: 'Mozarela',
+		cheese: 'Queijo',
+		joghurt: 'Iogurte',
+		milk: 'Leite',
+		camembert: 'Camembert',
+		pizza: 'Pizza',
+		chicken: 'Frango',
+		icecream: 'Sorvete',
+		frenchfries: 'Batata frita',
+		vegetables: 'Vegetais',
+		potatoes: 'Batatas',
+		sausages: 'Frios',
+		burgers: 'Hambúrguer',
+		pig: 'Carne de porco',
+		beef: 'Carne de bovina',
+		steak: 'Bife',
+		apples: 'Maçãs',
+		cherry: 'Cerejas',
+		banana: 'Bananas',
+		mango: 'Mangas',
+		oranges: 'Laranjas',
+		berrymix: 'Mistura de frutas vermelhas',
+		diymag: 'Revista DYI',
+		sportsillustrated: 'Revista de esportes',
+		businessmanager: 'Revista de gerente de negócios',
+		holisticsoul: 'Revista Alma holística',
+		intermediate: 'Revista Intermediária',
+		petshop: 'Revista Pet Shop',
+		ramen: 'Macarrão Ramen',
+		chocolate: 'Chocolate',
+		instantnoodles: 'Macarrão instantâneo',
+		bubblegum: 'Goma de mascar',
+		jellybaby: 'Ursinhos de gelatina',
+		candy: 'Doces'
+	}
+};
